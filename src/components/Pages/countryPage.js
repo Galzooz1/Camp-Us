72056a2d6c207e7fc4ff70bc1781abafc3a26acc
galaluf <galaf@elad.co.il>
2021-11-07 14:32:34 +0200

    country-page-key

--- src/components/Pages/countryPage.js
+++ src/components/Pages/countryPage.js
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { Image } from 'antd';
+import { Button, Image, Spin, Tooltip } from 'antd';
 import storeCountry from '../../stores/countryPageStore';
 import Header from '../HomePage/header';
 import { URL_API } from '../services/apiService';
@@ -19,6 +19,7 @@ min-height: 350px;
 `;
 
 const CountryPage = (props) => {
+    const [hotelKey, setHotelKey] = useState(0);
 
     useEffect(() => {
         let dataUrl = URL_API + "/countries/" + props.match.params.countryName;
@@ -28,6 +29,14 @@ const CountryPage = (props) => {
 
     console.log(toJS(storeCountry.countryData.hotels));
 
+    const starsRender = (numOfStars) => {
+        let stars = [];
+        for (let i = 0; i < numOfStars; i++) {
+            stars[i] = (<i className="fas fa-star text-warning"></i>)
+        }
+        return stars;
+    }
+
     return (
         <>
             <Header />
@@ -35,31 +44,45 @@ const CountryPage = (props) => {
                 <article>
                     <h1>{storeCountry.countryData?.name}</h1>
                 </article>
-                <hr />
+                <hr style={{ backgroundColor: "#263EA0", borderTop: "3px solid #263EA0" }} />
                 <div className="container mt-5">
                     <WrapperDiv>
                         <div className="col-lg-5">
-                            <h2 className="border-bottom">Hotels</h2>
-                            {toJS(storeCountry.countryData?.hotels?.values)?.length > 0 &&
+                            <h2 className="border-bottom text-white">Hotels</h2>
+                            {storeCountry.countryData?.hotels?.values?.length > 0 ?
                                 <>
-                                    {toJS(storeCountry.countryData?.hotels?.values).map((item, i) => {
+                                    {storeCountry.countryData?.hotels?.values.map((item, i) => {
                                         // console.log(item)
                                         return (
-                                            <AttractionDiv className="bg-success p-4">
+                                            <AttractionDiv key={i} className="bg-success p-4">
+                                                <h2>{item?.mapValue.fields.name.stringValue}</h2>
                                                 <h3>City: {item?.mapValue.fields.city.stringValue}</h3>
                                                 <h4>Address: {item?.mapValue.fields.address.stringValue}</h4>
-                                                <h4>Stars: {item?.mapValue.fields.stars.integerValue}</h4>
+                                                <h4>Stars:
+                                                    <Tooltip title={item?.mapValue.fields.stars.integerValue + " Stars"}>
+                                                     {starsRender(item?.mapValue.fields.stars.integerValue)}
+                                                    </Tooltip>
+                                                     </h4>
                                                 <Image.PreviewGroup>
                                                     <div className="mt-5 d-flex flex-wrap justify-content-around align-items-end">
-                                                        <Image width={100} src="https://via.placeholder.com/500x500" />
-                                                        <Image width={100} src="https://via.placeholder.com/500x500" />
-                                                        <Image width={100} src="https://via.placeholder.com/500x500" />
+                                                        {item?.mapValue.fields.imgs.arrayValue.values.map((img, i) => {
+                                                            return(
+                                                                <Image key={img.stringValue} width={120} height={100} src={img.stringValue} />
+                                                            )
+                                                        })}
+                                                        {/* <Image width={100} src="https://via.placeholder.com/500x500" /> */}
+                                                        {/* <Image width={100} src="https://via.placeholder.com/500x500" /> */}
                                                     </div>
                                                 </Image.PreviewGroup>
+                                                <Button onClick={() => setHotelKey(hotelKey + 1)} type="primary">Next Hotel <i className="ms-2 fas fa-arrow-right"></i></Button>
                                             </AttractionDiv>
                                         )
                                     })}
                                 </>
+                                :
+                                <div className="d-flex justify-content-center">
+                                <Spin />
+                                </div>
                             }
                         </div>
                         <div className="col-lg-5">
